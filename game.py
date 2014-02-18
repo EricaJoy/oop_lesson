34c92c0db4c9cbf60a18851439a552bd9ef97114
Hackbright Student <info@hackbrightacademy.com>
2014-02-18 12:22:43 -0800

    Characters that move and shit.

--- game.py
+++ game.py
@@ -11,13 +11,72 @@ KEYBOARD = None
 PLAYER = None
 ######################
 
-GAME_WIDTH = 3
-GAME_HEIGHT = 3
+GAME_WIDTH = 5
+GAME_HEIGHT = 5
 
 #### Put class definitions here ####
-pass
+class Rock(GameElement):
+    IMAGE = "Rock"
+
+class Character(GameElement):
+    IMAGE = "Princess"
+
 ####   End class definitions    ####
 
 def initialize():
     """Put game initialization code here"""
-    pass
+
+    global PLAYER
+    PLAYER = Character()
+    GAME_BOARD.register(PLAYER)
+    GAME_BOARD.set_el(2, 2, PLAYER)
+    print PLAYER
+
+
+    rock_positions = [
+        (2, 1),
+        (1, 2),
+        (3, 2),
+        (2, 3)
+    ]
+
+    rocks = []
+
+    for pos in rock_positions:
+        rock = Rock()
+        GAME_BOARD.register(rock)
+        GAME_BOARD.set_el(pos[0], pos[1], rock)
+        rocks.append(rock)
+
+    for rock in rocks:
+        print rock
+
+    # print "cobalt is at", (cobalt.x, cobalt.y)
+    # print "granite is at", (granite.x, granite.y)
+    # print "cobalt image", cobalt.IMAGE
+    # print "granite image", granite.IMAGE    
+
+def keyboard_handler():
+    if KEYBOARD[key.UP]:
+        GAME_BOARD.draw_msg("You pressed up")
+        next_y = PLAYER.y - 1
+        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+        GAME_BOARD.set_el(PLAYER.x, next_y, PLAYER)
+    elif KEYBOARD[key.DOWN]:
+        GAME_BOARD.draw_msg("You pressed down")
+        next_y = PLAYER.y + 1
+        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+        GAME_BOARD.set_el(PLAYER.x, next_y, PLAYER)
+    elif KEYBOARD[key.LEFT]:
+        GAME_BOARD.draw_msg("You pressed left")
+        next_x = PLAYER.x - 1
+        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+        GAME_BOARD.set_el(next_x, PLAYER.y, PLAYER) 
+    elif KEYBOARD[key.RIGHT]:
+        GAME_BOARD.draw_msg("You pressed right")
+        next_x = PLAYER.x + 1
+        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+        GAME_BOARD.set_el(next_x, PLAYER.y, PLAYER) 
+    elif KEYBOARD[key.SPACE]:
+        GAME_BOARD.erase_msg()
+
